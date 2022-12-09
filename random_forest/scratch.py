9b5958ad9967c6fb4871c5e168f4db9dae2334cc
allysonvasquez <allysonvasquez01@gmail.com>
2022-12-08 19:18:40 -0500

    With package complete

--- random_forest/scratch.py
+++
@@ -1,8 +0,0 @@
-# calculate the entropy for a dataset
-from math import log2
-
-# calculate the entropy for the split in the dataset
-def entropy(y_class, n_class):
-    return - (y_class * log2(y_class) + n_class * log2(n_class))
-
-entropy(class_bal['yes'], class_bal['no'])
